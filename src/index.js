e1eff1622f3f8db7f33ce7086f0ea31cafa707f2
Himanshu Jagtap <jagtaphimanshu09@gmail.com>
2020-03-20 21:11:40 +0530

    Added check_draw method

--- src/index.js
+++ src/index.js
@@ -78,6 +78,14 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        history: [
+            Array(9).fill(null);
+        ];
+    }
+  }
   render() {
     return (
       <div className="game">
