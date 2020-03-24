14b41e3fb38b890847e54d7ae75e4aab6daf07ad
Himanshu@372 <jagtaphimanshu09@gmail.com>
2020-03-24 10:45:10 +0530

    Improvements

--- src/index.js
+++ src/index.js
@@ -22,25 +22,23 @@ class Board extends React.Component {
           );
   }
 
+  renderBoard(i){
+      let squares = [];
+      for(let row = 0; row < i; row++) {
+          let rows = [];
+          for (let col = 0; col < i; col++) {
+              rows.push(this.renderSquare(i*row + col));
+          }
+          squares.push(<div className="board-row">{rows}</div>);
+      }
+      return(
+          squares
+      );
+  }
+
   render() {
     return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+      <div>{this.renderBoard(3)}</div>
     );
   }
 }
